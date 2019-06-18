60d134390bb2aff25960e8b93a470537cd427c87
med_moline <antonio.medugno1990@gmail.com>
2019-06-18 16:52:44 +0200

    creazione file per php javscript e sass

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <link rel="stylesheet" href="public/css/app.css">
+    <meta charset="utf-8">
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <title></title>
+  </head>
+  <body>
+     <script src="src/js/app.js" charset="utf-8"></script>
+  </body>
+</html>
